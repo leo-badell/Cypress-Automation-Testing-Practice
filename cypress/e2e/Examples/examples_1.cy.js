7afbf8c0d5d633a85d77cd1b66693a17c0ec9d4a
Leonardo Badell <leonardobadell@hotmail.com>
2024-05-14 22:41:55 -0300

    Adding variables to input-date

--- cypress/e2e/Examples/examples_1.cy.js
+++ cypress/e2e/Examples/examples_1.cy.js
@@ -18,7 +18,17 @@ beforeEach(() => {
     cy.get('#input-text').type('Another test')
     cy.get('#input-password').type('Cypress/e2e')
 
+    let date = new Date()
+    date.setDate(date.getDate() + 400)
+    let futureYear = date.getFullYear()
+    let futureMonth = ("0" + (date.getMonth() + 1)).slice(-2)
+    let futureDay = date.getDate()
+    let dateToAssert = `${futureYear}-${futureMonth}-${futureDay}`
 
+    cy.get('#input-date').type(dateToAssert)
+    cy.get('#btn-display-inputs').click()
+    cy.wait(1000)
+    cy.getByClass('btn-outline-danger').click()
    
   })
 })
