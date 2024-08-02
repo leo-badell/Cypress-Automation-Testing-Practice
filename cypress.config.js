29a89b6eacc61d770deb3f07a4d75676ed1d251c
Leonardo Badell <leonardobadell@hotmail.com>
2024-08-02 15:27:12 -0300

    Adding the second important branch with faker

--- cypress.config.js
+++ cypress.config.js
@@ -1,3 +1,4 @@
+const { defineConfig } = require('cypress');
 
 module.exports = {
   e2e: {
@@ -6,6 +7,16 @@ module.exports = {
     },
     setupNodeEvents(on, config) {
       // Intercept ad requests globally
+      module.exports = {
+        e2e: {
+          setupNodeEvents(on, config) {
+            // This is required if your project uses a file extension other than .js for test files
+            // (e.g., .ts, .jsx, etc.)
+            on('file:preprocessor', require('@cypress/typescript-preprocessor'));
+          },
+        },
+      };
+      
       on('before:browser:launch', (browser = {}, launchOptions) => {
         launchOptions.args.push('--disable-features=PreloadMediaEngagementData,AutoplayIgnoreWebAudio,PreloadMediaEngagementData2');
         return launchOptions;
--- cypress/support/e2e.js
+++ cypress/support/e2e.js
@@ -17,5 +17,6 @@
 import './commands'
 import 'cypress-plugin-api'
 
+
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
